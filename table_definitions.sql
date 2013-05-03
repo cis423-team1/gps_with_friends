5b6b8c9ed0fcd576107b2a2d28437d5885c53020
Joshua Burkhart <burkhart.joshua@gmail.com>
2013-05-02 19:56:01 -0700

    updated table defs to reflect Ahmad's comment..

--- table_definitions.sql
+++ table_definitions.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Track_History
 (
-    UID             INT             NOT NULL,
+    UID             INT,
     Data            DATE,
     Time            TIME,
     Location_x      FLOAT(32,30),
     Location_y      FLOAT(32,30),
     UserList_UID    INT,
-    FOREIGN KEY (UID) REFERENCES User(UID)
+    FOREIGN KEY (UserList_UID) REFERENCES User(UID)
 );
 
 CREATE TABLE User
@@ -14,7 +14,7 @@ CREATE TABLE User
     UID             INT             NOT NULL,
     Fname           VARCHAR(45),
     Lname           VARCHAR(45),
-    Email           VARCHAR(45)
+    Email           VARCHAR(45),
     PRIMARY KEY (UID)
 );
 
@@ -26,7 +26,8 @@ CREATE TABLE Location
     Location_x      FLOAT(32,30),
     Location_y      FLOAT(32,30),
     UserList_UID    INT,
-    PRIMARY KEY (UID)
+    PRIMARY KEY (UID),
+    FOREIGN KEY (UserList_UID) REFERENCES User(UID)
 );
 
 CREATE TABLE Group
@@ -35,22 +36,24 @@ CREATE TABLE Group
     Group_Name      VARCHAR(45),
     Owner_ID        INT,
     UserList_UID    INT,
-    PRIMARY KEY (Group_ID)
+    PRIMARY KEY (Group_ID),
+    FOREIGN KEY (UserList_UID) REFERENCES User(UID)
 );
 
 CREATE TABLE Group_Lists
 (
-    Group_ID        INT             NOT NULL,
+    Group_ID        INT,
     UID             INT,
     UserList_UID    INT,
-    FOREIGN KEY (Group_ID) REFERENCES Group(Group_ID)
+    FOREIGN KEY (Group_GroupID) REFERENCES Group(Group_ID)
 );
 
 CREATE TABLE Authentication
 (
-    UID_Tracker     INT             NOT NULL,
-    UID_Trackee     INT             NOT NULL,
+    UID_Tracker     INT,
+    UID_Trackee     INT,
     Data            DATE,       
     Time            TIME,
-    UserList_UID    INT
+    UserList_UID    INT,
+    FOREIGN KEY (User_UID) REFERENCES User(UID)
 );

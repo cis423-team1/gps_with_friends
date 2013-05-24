9c0d5092286f0c2faaa67744814168e5ef8c6708
juyingnan <bunny_gg@hotmail.com>
2013-05-24 13:22:30 +0800

    first connection with server

--- WP8Client/GPSWithFriends/GPSWithFriends/App.xaml.cs
+++ WP8Client/GPSWithFriends/GPSWithFriends/App.xaml.cs
@@ -139,6 +139,7 @@ namespace GPSWithFriends
             // Create the frame but don't set it as RootVisual yet; this allows the splash
             // screen to remain active until the application is ready to render.
             RootFrame = new PhoneApplicationFrame();
+            //RootFrame = new TransitionFrame();
             RootFrame.Navigated += CompleteInitializePhoneApplication;
 
             // Handle navigation failures
--- WP8Client/GPSWithFriends/GPSWithFriends/LoginPage.xaml.cs
+++ WP8Client/GPSWithFriends/GPSWithFriends/LoginPage.xaml.cs
@@ -18,8 +18,25 @@ namespace GPSWithFriends
         }
 
         private void LOGINBUTTON_Click(object sender, RoutedEventArgs e)
-        {
+        {            
             this.NavigationService.Navigate(new Uri("/MainPage.xaml",UriKind.Relative));
+        }
+
+        private void REGISTERBUTTON_Click(object sender, RoutedEventArgs e)
+        {
+            string email="bunny_gg@hotmail.com";
+            string password = "test";
+            string fname="yingnan";
+            string lname = "ju";
+            Server.GPSwfriendsClient proxy = new Server.GPSwfriendsClient();
+            proxy.registerCompleted += proxy_registerCompleted;
+            proxy.registerAsync(email, password, fname, lname);
+        }
+
+       void proxy_registerCompleted(object sender, Server.registerCompletedEventArgs e)
+        {
+            Server.status s = e.Result;
+            REGISTERBUTTON.Content = s.ToString();
         }
     }
 }
